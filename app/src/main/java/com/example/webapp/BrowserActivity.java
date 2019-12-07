b20c6a966e22faca27f72bd3d7891a5f6d10038e
Dellivi <destroyer255333@gmail.com>
2019-12-07 15:57:43 +0500

    Added searchView, some colors and strings. 07/12/19.

--- app/src/main/java/com/example/webapp/BrowserActivity.java
+++ app/src/main/java/com/example/webapp/BrowserActivity.java
@@ -2,19 +2,19 @@ package com.example.webapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class BrowserActivity extends AppCompatActivity {
-    private WebView webView = findViewById(R.id.web_view);
+    private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -22,12 +22,22 @@ public class BrowserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        webView = findViewById(R.id.web_view);
+        webView.setWebViewClient(new Callback());
+
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+            }
+        });
+
+
 
         Uri url = getIntent().getData();
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new Callback());
+
         webView.loadUrl(url.toString());
 
     }
@@ -47,23 +57,5 @@ public class BrowserActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        SearchView searchView = findViewById(R.id.action_find);
-        searchView.setSubmitButtonEnabled(true);
-        switch (item.getItemId()){
-            case R.id.refresh_btn:
-                webView.reload();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
