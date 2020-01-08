e43c59c638ae24c9b33ceda8f66294649adcbb68
Dellivi <destroyer255333@gmail.com>
2020-01-08 20:14:32 +0500

    Added Nested Scroll for Swipe Refresh. //TEST//. 08/01/20.

--- app/src/main/java/com/example/webapp/Activities/BrowserActivity.java
+++ app/src/main/java/com/example/webapp/Activities/BrowserActivity.java
@@ -1,10 +1,14 @@
 package com.example.webapp.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.annotation.SuppressLint;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -14,8 +18,9 @@ import android.widget.ProgressBar;
 import com.example.webapp.R;
 import com.example.webapp.WebClient.WebClient;
 
-public class BrowserActivity extends AppCompatActivity  {
+public class BrowserActivity extends AppCompatActivity implements SwipeRefreshLayout.OnChildScrollUpCallback{
 
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private WebView webView;
     private SearchView mSearchView;
     private ProgressBar pb;
@@ -23,15 +28,29 @@ public class BrowserActivity extends AppCompatActivity  {
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
 
+        mSwipeRefreshLayout = findViewById(R.id.swipeToRefresh_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+
         pb = findViewById(R.id.pb);
         mSearchView = findViewById(R.id.search_v);
-        setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
+
 
         webView = findViewById(R.id.web_view);
         webView.setWebViewClient(new WebClient());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            webView.getSettings().getSafeBrowsingEnabled();
+        }
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebAppChromeClient());
 
@@ -40,6 +59,13 @@ public class BrowserActivity extends AppCompatActivity  {
         if (urlU != null) {
             webView.loadUrl(urlU.toString());
         }
+
+
+    }
+
+    @Override
+    public boolean canChildScrollUp(@NonNull SwipeRefreshLayout parent, @Nullable View child) {
+        return false;
     }
 
 
@@ -53,6 +79,8 @@ public class BrowserActivity extends AppCompatActivity  {
             }else pb.setVisibility(View.VISIBLE);
         }
     }
+
+
 
     @Override
     public void onBackPressed() {
